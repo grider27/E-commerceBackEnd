25821d4c86ec08aef70a2b3d8b40a6f1bcb15e4d
Gary B <grider27@gmail.com>
2021-03-21 17:26:45 -0400

    product routes functional

--- models/index.js
+++ models/index.js
@@ -6,11 +6,14 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
-Product.hasOne(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
